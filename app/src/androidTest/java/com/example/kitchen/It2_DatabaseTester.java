2996a122f2d8ad69e1dee3b289009cce758a08e5
Michaela Stelse <michaelastelse@Michaelas-MacBook-Pro.local>
2019-12-05 17:07:47 -0600

    Got rid of some It2 tests that make no sense with the convertUnit methods updated

--- app/src/androidTest/java/com/example/kitchen/It2_DatabaseTester.java
+++ app/src/androidTest/java/com/example/kitchen/It2_DatabaseTester.java
@@ -1107,31 +1107,15 @@ public class It2_DatabaseTester {
     }
 
     /**
-     * This method checks that convertRecipeIngredientUnits returns a Recipe with the units correctly
-     * changes
-     * This method tests convertUnits was pounds, is now pinches
+     * This method checks that convertRecipeIngredientUnits returns an unchanged Recipe when trying
+     * to convert between two units that should not be converted between
      */
     @Test
     public void convertRecipeIngredientUnits_PoundsToPinches() {
         setUp();
         createRecipe_DifferentUnits("pound(s)");
         Recipe returned = testDatabase.convertRecipeIngredientUnits(testRecipe2, "pound(s)", "pinch(es)");
-        assertEquals("convertRecipeIngredientUnits_PintToCups - Quantity", 8834.56, returned.getIngredientList().get(0).getQuantity(), 0);
-        assertEquals("convertRecipeIngredientUnits_PintToCups - Unit", "pinch(es)", returned.getIngredientList().get(0).getUnit());
-        tearDown();
-    }
-
-    /**
-     * This method checks that convertRecipeIngredientUnits returns a Recipe with the units correctly
-     * changes
-     * This method tests convertUnits was pinches, is now pounds
-     */
-    @Test
-    public void convertRecipeIngredientUnits_PinchesToPounds() {
-        setUp();
-        createRecipe_DifferentUnits("pinch(es)");
-        Recipe returned = testDatabase.convertRecipeIngredientUnits(testRecipe2, "pinch(es)", "pound(s)");
-        assertEquals("convertRecipeIngredientUnits_PintToCups - Quantity", 0.00, returned.getIngredientList().get(0).getQuantity(), 0);
+        assertEquals("convertRecipeIngredientUnits_PintToCups - Quantity", 6, returned.getIngredientList().get(0).getQuantity(), 0);
         assertEquals("convertRecipeIngredientUnits_PintToCups - Unit", "pound(s)", returned.getIngredientList().get(0).getUnit());
         tearDown();
     }
