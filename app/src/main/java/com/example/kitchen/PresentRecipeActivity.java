31cf03e21100110573a912b2bbc9356e350322cb
schmude <m.s.0326>
2019-12-08 10:32:20 -0600

    Present Recipe

--- app/src/main/java/com/example/kitchen/PresentRecipeActivity.java
+++ app/src/main/java/com/example/kitchen/PresentRecipeActivity.java
@@ -2,13 +2,53 @@ package com.example.kitchen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class PresentRecipeActivity extends AppCompatActivity {
-
+    DatabaseHelper database = new DatabaseHelper(this);
+    Recipe recipe;
+    int position;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_present_recipe);
+
+        int recipeId = getIntent().getIntExtra("recipeId", -1);
+        recipe = database.getRecipe(recipeId);
+        textView = findViewById(R.id.present_recipe_text_view);
+        position = 0;
+        String string = (position + 1) + ". " + recipe.getDirectionsList().get(position).getDirectionText();
+        textView.setText(string);
+
+    }
+
+    public void onPresentRecipeNextButtonPressed(View view){
+        position++;
+        if(position < (recipe.getDirectionsList().size() - 1)){
+            String string = (position + 1) + ". " + recipe.getDirectionsList().get(position).getDirectionText();
+            textView.setText(string);
+        }else {
+            Intent intent = new Intent(this, DisplaySelectedRecipeActivity.class);
+            intent.putExtra("recipeId", recipe.getKeyID());
+            startActivity(intent);
+            this.finish();
+        }
+    }
+    public void onPresentRecipeBackButtonPressed(View view){
+        position--;
+        if(position > -1){
+            String string = (position + 1) + ". " + recipe.getDirectionsList().get(position).getDirectionText();
+            textView.setText(string);
+        }else {
+            Intent intent = new Intent(this, DisplaySelectedRecipeActivity.class);
+            intent.putExtra("recipeId", recipe.getKeyID());
+            startActivity(intent);
+            this.finish();
+        }
     }
 }
